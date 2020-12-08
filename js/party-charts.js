7a4d7a3597e6010e4e042e748ed4c4ace2f077ab
Joren Vrancken <jorenvrancken@gmail.com>
2020-12-08 21:57:11 +0100

    Add party specific spending charts

--- js/party-charts.js
+++ js/party-charts.js
@@ -6,172 +6,79 @@ $(document).ready(function () {
         let searchParams = new URLSearchParams(window.location.search)
 
         if (!searchParams.has("party")) {
-            $('#party-specific-charts').append("<p>No Party Provided.</p>");
+            $("#party-specific-charts").append("<p>No Party Provided.</p>");
             return;
         }
 
         let party = searchParams.get("party");
 
         if (!Object.keys(adData["party-specific-data"]).includes(party)) {
-            $('#party-specific-charts').append("<p>Party Not Found.</p>");
+            $("#party-specific-charts").append("<p>Party Not Found.</p>");
             return;
         }
 
-        let regionLineChartCanvasId = party.toLowerCase() + "-region-line-chart";
-        let regionDoughnutChartCanvasId = party.toLowerCase() + "-region-doughnut-chart";
+        let partySpecificCharts = [
+            {"labelType": "Region", "dataType": "Spending"},
+            {"labelType": "Gender", "dataType": "Spending"},
+            {"labelType": "Age", "dataType": "Spending"},
+        ];
 
-        let genderLineChartCanvasId = party.toLowerCase() + "-gender-line-chart";
-        let genderDoughnutChartCanvasId = party.toLowerCase() + "-gender-doughnut-chart";
+        partySpecificCharts.forEach(function (element){
 
-        let ageLineChartCanvasId = party.toLowerCase() + "-age-line-chart";
-        let ageDoughnutChartCanvasId = party.toLowerCase() + "-age-doughnut-chart";
+            let labelType = element["labelType"];
+            let dataType = element["dataType"];
 
-        let genderAgeLineChartCanvasId = party.toLowerCase() + "-gender-age-line-chart";
-        let genderAgeDoughnutChartCanvasId = party.toLowerCase() + "-gender-age-doughnut-chart";
+            let LineChartCanvasId = party.toLowerCase() + "-" + labelType.toLowerCase() + "-" + dataType.toLowerCase() + "-line-chart";
+            let DoughnutChartCanvasId = party.toLowerCase()+ "-" + labelType.toLowerCase() + "-" + dataType.toLowerCase() +  "-doughnut-chart";
 
-        let partyChartsHTML = `<div id="${party.toLowerCase()}-charts">
-            <div class="text-center">
-                <h2>${party} Charts</h2>
-            </div>
-            <div>
-                 <div>
+            $("#party-specific-charts").append(
+                `<div>
                     <div class="text-center">
-                        <h3>Impressions per Region (${party})</h3>
+                        <h3>Estimated ${dataType} per ${labelType} (${party})</h3>
                     </div>
                     
                     <div class="row">
                         <div class="col-8">
-                            <canvas id="${regionLineChartCanvasId}"></canvas>
+                            <canvas id="${LineChartCanvasId}"></canvas>
                         </div>
                         <div class="col-4">
-                            <canvas id="${regionDoughnutChartCanvasId}"></canvas>
+                            <canvas id="${DoughnutChartCanvasId}"></canvas>
                         </div>
                     </div>
                 </div>
+                <hr>`);
 
-                <hr>
-                <div>
-                    <div class="text-center">
-                        <h3>Impressions per Gender (${party})</h3>
-                    </div>
-                    
-                    <div class="row">
-                        <div class="col-8">
-                            <canvas id="${genderLineChartCanvasId}"></canvas>
-                        </div>
-                        <div class="col-4">
-                            <canvas id="${genderDoughnutChartCanvasId}"></canvas>
-                        </div>
-                    </div>
-                </div>
-                <hr>
 
-                <div>
-                    <div class="text-center">
-                        <h3>Impressions per Age (${party})</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <canvas id="${ageLineChartCanvasId}"></canvas>
-                        </div>
-                        <div class="col-4">
-                            <canvas id="${ageDoughnutChartCanvasId}"></canvas>
-                        </div>
-                    </div>
-                </div>
+            let lineChart = generateLineGraphConfig(adData,
+                "Estimated " + dataType + " per " + labelType + " per Day ("+ party + ")",
+                dataType.toLowerCase() + "-per-" + labelType.toLowerCase() + "-per-date",
+                party);
 
-                <hr>
-
-                <div>
-                    <div class="text-center">
-                        <h3>Impressions per Gender and Age (${party})</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <canvas id="${genderAgeLineChartCanvasId}"></canvas>
-                        </div>
-                        <div class="col-4">
-                            <canvas id="${genderAgeDoughnutChartCanvasId}"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-        </div>`;
-
-        $('#party-specific-charts').append(partyChartsHTML);
-
-        let yAxesPercentage = [{
-            ticks: {
-                callback: function (value, index, values) {
-                    return value + "%";
+            lineChart.options.scales.yAxes = [{
+                ticks: {
+                    callback: function (value, index, values) {
+                        return "€" + value;
+                    }
                 }
-            }
-        }];
-        let tooltipsPercentage = {
-            mode: 'x',
-            intersect: false,
-            callbacks: {
-                label: function (tooltipItems, data) {
-                    return data.datasets[tooltipItems.datasetIndex].label + ": " + tooltipItems.yLabel + "%";
-                },
+            }];
+            lineChart.options.tooltips = {
+                mode: "x",
+                intersect: false,
+                callbacks: {
+                    label: function (tooltipItems, data) {
+                        return data.datasets[tooltipItems.datasetIndex].label + ": €" + tooltipItems.yLabel;
+                    },
 
-            }
-        };
+                }
+            };
+            new Chart($("#" + LineChartCanvasId), lineChart);
 
-        // Line chart for party impressions per region per day
-        let partyRegionLineChart = generateLineGraphConfig(adData,
-            "Impressions per Region per Day (" + party + ")",
-            "impressions-per-region-per-date",
-            party);
-        partyRegionLineChart.options.scales.yAxes = yAxesPercentage;
-        partyRegionLineChart.options.tooltips = tooltipsPercentage;
-        new Chart($("#" + regionLineChartCanvasId), partyRegionLineChart);
+            let doughnutChart = generateDoughnutChart(adData,
+                "Estimated " + dataType + " per " + labelType + " (" + party + ")",
+                dataType.toLowerCase() + "-per-" + labelType.toLowerCase(),
+                party)
+            new Chart($("#" + DoughnutChartCanvasId), doughnutChart);
+        });
 
-        // Doughnut chart for party impressions per region
-        let partyRegionDoughnutChart = generateDoughnutChart(adData, "Impressions per Region (" + party + ")", "impressions-per-region", party)
-        new Chart($("#" + regionDoughnutChartCanvasId), partyRegionDoughnutChart);
-
-
-        // Line chart for party impressions per gender per day
-        let partyGenderLineChart = generateLineGraphConfig(adData,
-            "Impressions per Gender per Day (" + party + ")",
-            "impressions-per-gender-per-date",
-            party);
-        partyGenderLineChart.options.scales.yAxes = yAxesPercentage;
-        partyRegionLineChart.options.tooltips = tooltipsPercentage;
-        new Chart($("#" + genderLineChartCanvasId), partyGenderLineChart);
-
-        // Doughnut chart for party impressions per gender
-        let partyGenderDoughnutChart = generateDoughnutChart(adData, "Impressions per Gender (" + party + ")", "impressions-per-gender", party)
-        new Chart($("#" + genderDoughnutChartCanvasId), partyGenderDoughnutChart);
-
-
-        // Line chart for party impressions per age per day
-        let partyAgeLineChart = generateLineGraphConfig(adData,
-            "Impressions per Age per Day (" + party + ")",
-            "impressions-per-age-per-date",
-            party);
-        partyAgeLineChart.options.scales.yAxes = yAxesPercentage;
-        partyAgeLineChart.options.tooltips = tooltipsPercentage;
-        new Chart($("#" + ageLineChartCanvasId), partyAgeLineChart);
-
-        // Doughnut chart for party impressions per age per day
-        let partyAgeDoughnutChart = generateDoughnutChart(adData, "Impressions per Age (" + party + ")", "impressions-per-age", party)
-        new Chart($("#" + ageDoughnutChartCanvasId), partyAgeDoughnutChart);
-
-
-        // Party impressions per gender and age per day
-        let partyGenderAgeLineChart = generateLineGraphConfig(adData,
-            "Impressions per Gender and Age per Day (" + party + ")",
-            "impressions-per-gender-and-age-per-date",
-            party);
-        partyGenderAgeLineChart.options.scales.yAxes = yAxesPercentage;
-        partyGenderAgeLineChart.options.tooltips = tooltipsPercentage;
-        new Chart($("#" + genderAgeLineChartCanvasId), partyGenderAgeLineChart);
-
-        // Doughnut chart for party impressions per age per day
-        let partyGenderAgeDoughnutChart = generateDoughnutChart(adData, "Impressions per Gender and Age(" + party + ")", "impressions-per-gender-and-age", party)
-        new Chart($("#" + genderAgeDoughnutChartCanvasId), partyGenderAgeDoughnutChart);
     });
 });
