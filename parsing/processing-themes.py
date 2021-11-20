96261970d4bcc422db96713c24464fe84c40a5e6
Joren Vrancken <jorenvrancken@gmail.com>
2021-11-20 11:47:08 +0100

    Fix black

--- parsing/processing-themes.py
+++ parsing/processing-themes.py
@@ -1,5 +1,9 @@
-
-from constants import FIRST_DATE, PARTIES, DEMOGRAPHIC_TYPES, DEMOGRAPHIC_TYPE_TO_LIST_MAP
+from constants import (
+    FIRST_DATE,
+    PARTIES,
+    DEMOGRAPHIC_TYPES,
+    DEMOGRAPHIC_TYPE_TO_LIST_MAP,
+)
 
 from themes import Theme
 
@@ -13,50 +17,52 @@ THEME_NAMES = [t.name for t in Theme if t != Theme.NONE]
 if __name__ == "__main__":
 
     theme_data = {
-
         "impressions-demographics-theme": {
-            t: {
-                dt: [] for dt in DEMOGRAPHIC_TYPES
-            } for t in THEME_NAMES
+            t: {dt: [] for dt in DEMOGRAPHIC_TYPES} for t in THEME_NAMES
         },
-
         "impressions-demographics-theme-party": {
-            p: {
-                t: {
-                    dt: [] for dt in DEMOGRAPHIC_TYPES
-                } for t in THEME_NAMES
-            } for p in PARTIES
+            p: {t: {dt: [] for dt in DEMOGRAPHIC_TYPES} for t in THEME_NAMES}
+            for p in PARTIES
         },
-
-        "impressions-theme-party": {
-            p: [] for p in PARTIES
-        },
-
-        "number-of-ads-theme-party": {
-            p: [] for p in PARTIES
-        },
+        "impressions-theme-party": {p: [0] * len(THEMES) for p in PARTIES},
+        "number-of-ads-theme-party": {p: [0] * len(THEMES) for p in PARTIES},
     }
 
     for theme in THEMES:
 
-        ads = Ad.select().where(Ad.start_date >= FIRST_DATE).where(Ad.themes.bin_and(theme.value) == theme.value)
+        ads = (
+            Ad.select()
+            .where(Ad.start_date >= FIRST_DATE)
+            .where(Ad.themes.bin_and(theme.value) == theme.value)
+        )
         for demographic_type in DEMOGRAPHIC_TYPES:
             for demographic in DEMOGRAPHIC_TYPE_TO_LIST_MAP[demographic_type]:
-                theme_data["impressions-demographics-theme"][theme.name][demographic_type].append(
-                    sum(a.rank_to_data("impressions", demographic) for a in ads)
-                )
+                theme_data["impressions-demographics-theme"][theme.name][
+                    demographic_type
+                ].append(sum(a.rank_to_data("impressions", demographic) for a in ads))
 
         for party in PARTIES:
-            ads = Ad.select().where(Ad.start_date >= FIRST_DATE).where(Ad.party == party).where(Ad.themes.bin_and(theme.value) == theme.value)
+            ads = (
+                Ad.select()
+                .where(Ad.start_date >= FIRST_DATE)
+                .where(Ad.party == party)
+                .where(Ad.themes.bin_and(theme.value) == theme.value)
+            )
 
             theme_data["number-of-ads-theme-party"][party].append(ads.count())
-            theme_data["impressions-theme-party"][party].append(sum(a.rank_to_data("impressions", "total") for a in ads))
+            theme_data["impressions-theme-party"][party].append(
+                sum(a.rank_to_data("impressions", "total") for a in ads)
+            )
 
             for demographic_type in DEMOGRAPHIC_TYPES:
                 for demographic in DEMOGRAPHIC_TYPE_TO_LIST_MAP[demographic_type]:
-                    theme_data["impressions-demographics-theme-party"][party][theme.name][demographic_type].append(
+                    theme_data["impressions-demographics-theme-party"][party][
+                        theme.name
+                    ][demographic_type].append(
                         sum(a.rank_to_data("impressions", demographic) for a in ads)
                     )
 
     recursive_round(theme_data)
-    render_template("themes.html", "themes.html", theme_data=theme_data, THEMES=THEME_NAMES)
+    render_template(
+        "themes.html", "themes.html", theme_data=theme_data, THEMES=THEME_NAMES
+    )
